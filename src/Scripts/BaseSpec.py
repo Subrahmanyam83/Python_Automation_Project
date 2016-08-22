8a6f1005f4f7b7e0e787ebcedf2f6f1ac22b9cff
E002642 <bhadrikishore02@gmail.com>
2016-08-22 17:26:19 +0530

    Merge branch 'master' of https://github.com/Subrahmanyam83/Python_Automation_Project into subu
    # Conflicts:
    #	src/Scripts/BaseSpec.py

--- src/Scripts/BaseSpec.py
+++ src/Scripts/BaseSpec.py
@@ -1,16 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.remote.webelement import WebElement
+
+class BaseSpec(object):
+
+    global webdriver
+    def _init_(self,driver=webdriver):
+        print('This is Sample Module Class')
+
+    def setUp(self,BrowserName):
+        self.BrowserName=BrowserName
+        if(self.BrowserName=="Firefox"):
+            self.driver = webdriver.Firefox()
+        elif(self.BrowserName=="Chrome"):
+            self.driver=webdriver.Chrome("C:\BrowserDriver\chromedriver_win32\chromedriver.exe")
+        elif(self.BrowserName=="Chrome"):
+            self.driver=webdriver.Chrome("C:\BrowserDriver\IEDriverServer_x64_2.53.1\IEDriverServer.exe")
+        else:
+            print("Please specify Browser correctly")
+
+    def Test_URL_Invoke(self,URL):
+        self.driver.get(URL)
+
+    def tearDown(self):
+        self.driver.close()
 
 
 
-class BaseClass(object):
-
-    def browserLaunch(self):
-        self.driver = webdriver.Firefox()
-        self.driver.maximize_window()
-        self.driver.get("http://www.facebook.com")
-        return self.driver
-
-    def clickElementByXpath(self,WebElement):
-        print()
 
